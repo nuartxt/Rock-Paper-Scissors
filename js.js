7d4470397769a5629402302a722ec20e27d94cc0
Anuar <151063105+nuartxt@users.noreply.github.com>
2025-10-04 13:28:39 +0500

    Add loops for function

--- js.js
+++ js.js
@@ -1,6 +1,13 @@
 let humanScore = 0;
 let computerScore = 0;
 
+let winner = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper"
+}
+
+
 function getComputerChose() {
     let array = ["rock", "paper", "scissors"];
     let index = Math.floor(Math.random() * array.length);
@@ -9,31 +16,31 @@ function getComputerChose() {
 }
 
 function getHumanChoice() {
-    let human_chose = prompt("Add num");
+    let human_chose = prompt("Enter scissors, rock or paper");
     console.log("human:" + human_chose);
     return human_chose;
 }
 
-let winner = {
-    rock: "scissors",
-    paper: "rock",
-    scissors: "paper"
-}
-
-let computer = getComputerChose();
-let human = getHumanChoice();
-
-
 //add comment for logic :)
-
 function playRound(human, computer) {
     if (human === computer) {
         return "draw";
     } else if (winner[human] === computer) {
+        humanScore++
         return "human win";
     } else {
+        computerScore++;
         return "computer win";
     }
 }
 
-console.log(playRound(human, computer))
+//loops for game loops
+for (let i = 0; i < 5; i++) {
+    let computer = getComputerChose();
+    let human = getHumanChoice();
+    console.log(playRound(human, computer))
+}
+
+//our result
+// console.log(playRound(human, computer))
+console.log(`score - Human:${humanScore}, Computer:${computerScore}`);
